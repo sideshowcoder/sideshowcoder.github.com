7fd8b1273994084d4907bf9bb28066aec528bcbe
Philipp Fehre <philipp.fehre@googlemail.com>
2013-04-02 22:01:21 +0200

    initial source branch

---
+++ Gemfile
@@ -0,0 +1,5 @@
+source "https://rubygems.org"
+
+gem "stasis"
+gem "trollop"
+gem "rb-fsevent" if RUBY_PLATFORM =~ /darwin/
---
+++ controller.rb
@@ -0,0 +1,6 @@
+# ignore everything needed to build but not to serve
+ignore /Gemfile.*/
+ignore /bin/
+ignore /Rakefile/
+
+
